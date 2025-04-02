e712d37d78ee703542f5d509c5c14b4e46a56391
sofia17042007 <sofiaguzhva19@gmail.com>
2025-04-02 19:16:09 +0300

    3

--- src/pl/pp/myFirstApp.java
+++ src/pl/pp/myFirstApp.java
@@ -1,7 +1,33 @@
 package pl.pp;
 
+import java.util.Scanner;
+
 public class myFirstApp {
     public static void main(String[] args) {
-        System.out.println("Hello Basics of Programming");
+        double x = 10;
+        double y = 2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        var result = x+y;
+        System.out.println("x + y = " + result);
+
+        result = x - y;
+        System.out.println("x - y = " + result);
+
+        result = x * y;
+        System.out.println("x * y = " + result);
+
+        result = x / y;
+        System.out.println("x / y = " + result);
+
+        result = x % y;
+        System.out.println("x % y = " + result);
+
+        System.out.println("Enter two numbers separated by the Enter key");
+        double first = scanner.nextDouble();
+        double second = scanner.nextDouble();
+
+        System.out.println("x + y = " + (first + second));
     }
 }
