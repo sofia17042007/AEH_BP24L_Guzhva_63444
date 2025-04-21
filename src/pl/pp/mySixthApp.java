bf0d0f1ff2f536dd1d98eff57383c64b6b6954f8
sofia17042007 <sofiaguzhva19@gmail.com>
2025-04-21 21:24:59 +0300

    First commit to Lab6 repository

---
+++ src/pl/pp/mySixthApp.java
@@ -0,0 +1,38 @@
+public class myFifthApp {
+
+    public static void main(String[] args) {
+        int n = 20; // You can change this number to test other values
+
+        // Measure time for iterative method
+        long startIterative = System.nanoTime();
+        long resultIterative = factorialIterative(n);
+        long endIterative = System.nanoTime();
+        System.out.println("Iterative factorial of " + n + " = " + resultIterative);
+        System.out.println("Time taken (iterative): " + (endIterative - startIterative) + " ns");
+
+        // Measure time for recursive method
+        long startRecursive = System.nanoTime();
+        long resultRecursive = factorialRecursive(n);
+        long endRecursive = System.nanoTime();
+        System.out.println("Recursive factorial of " + n + " = " + resultRecursive);
+        System.out.println("Time taken (recursive): " + (endRecursive - startRecursive) + " ns");
+    }
+
+    // Iterative method to calculate factorial
+    static long factorialIterative(int n) {
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // Recursive method to calculate factorial
+    static long factorialRecursive(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * factorialRecursive(n - 1);
+    }
+}
+
