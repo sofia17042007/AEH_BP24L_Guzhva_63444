25093a132a805f3f3c2bd3d5a595d4d5c8411898
sofia17042007 <sofiaguzhva19@gmail.com>
2025-05-22 11:18:50 +0200

    Lab7: implemented Person class and methods

--- src/pl/pp/mySeventhApp.java
+++ src/pl/pp/mySeventhApp.java
@@ -1,42 +1,26 @@
 package pl.pp;
-
-//7
-
 public class mySeventhApp {
-
     public static void main(String[] args) {
-        int n = 20; // You can change this number to test other values
 
-        // Measure time for iterative method
-        long startIterative = System.nanoTime();
-        long resultIterative = factorialIterative(n);
-        long endIterative = System.nanoTime();
-        System.out.println("Iterative factorial of " + n + " = " + resultIterative);
-        System.out.println("Time taken (iterative): " + (endIterative - startIterative) + " ns");
+        Person person = new Person("John", 25, "New York", 1999);
 
-        // Measure time for recursive method
-        long startRecursive = System.nanoTime();
-        long resultRecursive = factorialRecursive(n);
-        long endRecursive = System.nanoTime();
-        System.out.println("Recursive factorial of " + n + " = " + resultRecursive);
-        System.out.println("Time taken (recursive): " + (endRecursive - startRecursive) + " ns");
-    }
 
-    // Iterative method to calculate factorial
-    static long factorialIterative(int n) {
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
+        person.sayHello();
 
-    // Recursive method to calculate factorial
-    static long factorialRecursive(int n) {
-        if (n <= 1) {
-            return 1;
-        }
-        return n * factorialRecursive(n - 1);
+
+        System.out.println("Current age: " + person.getAge());
+
+
+        person.growOld(10);
+        System.out.println("After growOld(10): " + person.getAge());
+
+
+        person.beYounger();
+        System.out.println("After beYounger(): " + person.getAge());
+
+
+        System.out.println("Address: " + person.getAddress());
+        System.out.println("Year of birth: " + person.getYearOfBirth());
     }
 }
 
